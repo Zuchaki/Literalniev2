76e064da8faa1d57830bd7a7fc6cc70a817aa2fb
Bartłomiej Legierski <57797064+Zuchaki@users.noreply.github.com>
2023-11-27 08:41:31 +0100

    commit

--- services/config.ts
+++
@@ -1,7 +0,0 @@
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-export const apiRoutes = {
-  literalnies: {
-    _root: `${apiUrl}/literalnies/`,
-  },
-};
--- utils/findEndOfArray.ts
+++
@@ -1,9 +0,0 @@
-import { Status, Word } from "@/pages";
-
-export const endOfArray = (array: Word[]) => {
-  const currentTable = array.length - 1;
-  return {
-    outer: currentTable,
-    inner: array[currentTable]?.length ?? 0,
-  };
-};
